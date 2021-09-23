135b9ede0f5227adb1fc1b81263f3a966b2e9530
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2021-09-23 17:10:33 -0500

    Agregar archivos de proyecto.

---
+++ Data/DDetallePedido.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Data
+{
+    public class DDetallePedido
+    {
+        public List<DetallePedido> GetDetallePedidos (DetallePedido detallePedido)
+        {
+            SqlParameter[] parameters = null;
+            string comandText = string.Empty;
+            List<DetallePedido> detallePedidos = null;
+
+            try
+            {
+                comandText = "USP_GetDetallePedidos";
+                parameters = new SqlParameter[1];
+                parameters[0] = new SqlParameter("@IdPedido", SqlDbType.Int);
+                parameters[0].Value = detallePedido.Pedido.IdPedido;
+                detallePedidos = new List<DetallePedido>();
+
+                using (SqlDataReader reader = SqlHelper.ExecuteReader(SqlHelper.Connection, comandText,
+                    CommandType.StoredProcedure, parameters))
+                {
+                    while (reader.Read())
+                    {
+                        detallePedidos.Add(new DetallePedido
+                        {
+                            Pedido = detallePedido.Pedido,
+                            IdProducto = reader["idproducto"] != null ? Convert.ToInt32(reader["idproducto"]) : 0,
+                            PrecioUnidad = reader["preciounidad"] != null ? Convert.ToDecimal(reader["preciounidad"]) : 0,
+                            Cantidad = reader["cantidad"] != null ? Convert.ToInt32(reader["cantidad"]) : 0,
+                            Descuento = reader["descuento"] != null ? Convert.ToDecimal(reader["descuento"]) : 0
+                        });
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return detallePedidos;
+        }
+    }
+}
---
+++ WpfSemana05/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+
+namespace WpfSemana05
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            BPedido bPedido = null;
+            try
+            {
+                bPedido = new BPedido();
+                dgvPedido.ItemsSource = bPedido.GetPedidosEntreFechas(Convert.ToDateTime(txtFechaInicio.Text),
+                                                                      Convert.ToDateTime(txtFechaFin.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                bPedido = null;
+            }
+        }
+
+        private void dgvPedido_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void dgvPedido_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            Pedido row = (Pedido)dgvPedido.SelectedItem;
+            BDetallePedido bDetallePedido = null;
+            try
+            {
+                bDetallePedido = new BDetallePedido();
+                dgvDetallePedido.ItemsSource = bDetallePedido.GetDetallePedidos(row.IdPedido);
+                txtTotal.Text = bDetallePedido.GetDetalleTotalPorId(row.IdPedido).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                bDetallePedido = null;
+            }
+        }
+    }
+}
